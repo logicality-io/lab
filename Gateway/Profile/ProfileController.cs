30b377c73a2d313a269760d19231681331bb2245
Damian Hickey <dhickey@gmail.com>
2021-12-16 22:19:52 +0100

    wip

---
+++ Gateway/Profile/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Profile;
+
+[Authorize("RequireInteractiveUser")]
+public class ProfileController : ControllerBase
+{
+    // GET
+    [HttpGet("profile")]
+    public IActionResult Get()
+    {
+        var userClaims = HttpContext.User.Claims.ToArray();
+        var serializedClaims = userClaims.Select(x =>
+            new
+            {
+                Type  = x.Type,
+                Value = x.Value
+            });
+        return Ok(serializedClaims);
+    }
+}
